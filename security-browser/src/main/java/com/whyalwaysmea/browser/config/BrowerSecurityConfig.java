6556902026adcad7d65664204b82b4677945205b
404402223@qq.com <404402223@qq.com>
2018-03-21 21:40:51 +0800

    图形验证码

--- security-browser/src/main/java/com/whyalwaysmea/browser/config/BrowerSecurityConfig.java
+++ security-browser/src/main/java/com/whyalwaysmea/browser/config/BrowerSecurityConfig.java
@@ -2,6 +2,7 @@ package com.whyalwaysmea.browser.config;
 
 import com.whyalwaysmea.browser.authentication.MyAuthenctiationFailureHandler;
 import com.whyalwaysmea.core.properties.SecurityProperties;
+import com.whyalwaysmea.core.validate.ValidateCodeFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @Author: HanLong
@@ -36,14 +38,21 @@ public class BrowerSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()                    //  定义当需要用户登录时候，转到的登录页面。
+        ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
+        validateCodeFilter.setAuthenticationFailureHandler(myAuthenticationFailureHandler);
+
+        http.addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
+                .formLogin()                    //  定义当需要用户登录时候，转到的登录页面。
                 .loginPage("/authentication/require")           // 设置登录页面
                 .loginProcessingUrl("/user/login")              // 自定义的登录接口
                 .successHandler(myAuthenticationSuccessHandler) // 自定义登录成功处理
                 .failureHandler(myAuthenticationFailureHandler) // 自定义登录失败处理
                 .and()
                 .authorizeRequests()        // 定义哪些URL需要被保护、哪些不需要被保护
-                .antMatchers("/authentication/require", securityProperties.getBrowser().getLoginPage()).permitAll()     // 设置所有人都可以访问登录页面
+                .antMatchers("/authentication/require",
+                        securityProperties.getBrowser().getLoginPage(),
+                        "/code/image")
+                .permitAll()     // 设置所有人都可以访问登录页面
                 .anyRequest()               // 任何请求,登录后可以访问
                 .authenticated()
                 .and()
---
+++ security-core/src/main/java/com/whyalwaysmea/core/validate/ValidateCodeFilter.java
@@ -0,0 +1,81 @@
+package com.whyalwaysmea.core.validate;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.social.connect.web.HttpSessionSessionStrategy;
+import org.springframework.social.connect.web.SessionStrategy;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author: HanLong
+ * @Date: Create in 2018/3/21 21:04
+ * @Description:
+ */
+@Component
+public class ValidateCodeFilter extends OncePerRequestFilter {
+
+    private SessionStrategy sessionStrategy = new HttpSessionSessionStrategy();
+
+    private AuthenticationFailureHandler authenticationFailureHandler;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        if(StringUtils.equals("/user/login", httpServletRequest.getRequestURI())
+                && StringUtils.equalsIgnoreCase(httpServletRequest.getMethod(), "post")) {
+
+            try {
+                validate(new ServletWebRequest(httpServletRequest));
+            } catch (ValidateCodeException e) {
+                authenticationFailureHandler.onAuthenticationFailure(httpServletRequest, httpServletResponse, e);
+                return ;
+            }
+
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    /**
+     * 图形验证码验证
+     * @param request
+     * @throws ServletRequestBindingException
+     */
+    private void validate(ServletWebRequest request) throws ServletRequestBindingException {
+        ImageCode codeInSession = (ImageCode) sessionStrategy.getAttribute(request, ValidateCodeController.SESSION_KEY);
+
+        String codeInRequest = ServletRequestUtils.getStringParameter(request.getRequest(), "imageCode");
+
+        if(StringUtils.isBlank(codeInRequest)) {
+            throw new ValidateCodeException("验证码的值不能为空");
+        }
+
+        if(codeInRequest == null) {
+            throw new ValidateCodeException("验证码不存在");
+        }
+
+        if(codeInSession.isExpried()) {
+            sessionStrategy.removeAttribute(request, ValidateCodeController.SESSION_KEY);
+            throw new ValidateCodeException("验证码已过期");
+        }
+
+        if(!StringUtils.equals(codeInSession.getCode(), codeInRequest)) {
+            throw new ValidateCodeException("验证码不匹配");
+        }
+
+        sessionStrategy.removeAttribute(request, ValidateCodeController.SESSION_KEY);
+    }
+
+    public void setAuthenticationFailureHandler(AuthenticationFailureHandler authenticationFailureHandler) {
+        this.authenticationFailureHandler = authenticationFailureHandler;
+    }
+}
